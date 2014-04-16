c203a5061622990d3c7b41cdb6f39b03286446f0
iluxonchik <iluxon4ik@hotmail.com>
2014-04-16 19:48:35 +0100

    Project provided files added.

---
+++ metaforms14.pl
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%
+%       GRUPO NUM: 
+%       ALUNOS: 
+%
+%               ATENCAO: NAO USAR ACENTOS OU CEDILHAS
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% ESCREVER AQUI O CODIGO DO PROJECTO DE LP 2013/2014
