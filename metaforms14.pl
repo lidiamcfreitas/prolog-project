ca4e92290f168f942a882cd95e23b0369257e196
iluxonchik <iluxon4ik@hotmail.com>
2014-05-06 06:48:34 +0100

    Added new predicates.
    adicionaPecasUsadas/1 adds the figure to the used figures "list".
    pecasNaoUsadas/1 unifies with non-used figures.

--- metaforms14.pl
+++ metaforms14.pl
@@ -10,6 +10,21 @@
 
 
 % ESCREVER AQUI O CODIGO DO PROJECTO DE LP 2013/2014
+
+% predicados auxiliares
+membro(X, [X|_]).
+membro(X, [_|R]) :- membro(X, R).
+
+% lista das precas disponiveis
+todasPecas([peca(triangulo, azul), peca(circulo, amarelo), peca(circulo, azul),peca(triangulo, amarelo), peca(triangulo, vermelho), peca(quadrado, vermelho), 
+ peca(quadrado, amarelo), peca(quadrado, azul), peca(circulo, vermelho)]).
+
+% adiciona a peca as "lista" das pecas usadas
+adicionaPecaUsadas(X):- assert(X).
+
+% mostra as pecas que ainda nao foram usadas
+pecasNaoUsadas(X) :- todasPecas(Y), ! , membro(X, Y), not(X).
+
 % o predicado coloca/4 coloca a peca numa posicao do tabuleiro
 coloca(A, top, left, [A,_,_,_,_,_,_,_,_]).
 coloca(A, top, middle, [_,A,_,_,_,_,_,_,_]).
@@ -21,20 +36,21 @@ coloca(A, bottom, left, [_,_,_,_,_,_,A,_,_]).
 coloca(A, bottom, middle, [_,_,_,_,_,_,_,A,_]).
 coloca(A, bottom, right, [_,_,_,_,_,_,_,_,A]).
 
+
 % implementacoes das pistas
-trioLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-trioLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-cobra(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-tSimples(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-tLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-tRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-tInvertido(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-cantoTopLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-cantoTopRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-cantoBottomLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-cantoBottomRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-diagonalGrave(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
-diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro).
+trioLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+trioLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+cobra(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+tSimples(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+tLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+tRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+tInvertido(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+cantoTopLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+cantoTopRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+cantoBottomLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+cantoBottomRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+diagonalGrave(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % TODO:
 % - ter uma lista que contem todas as pecas possiveis, sempre que
@@ -42,3 +58,8 @@ diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tab
 %	|OU| uma lista que faca o tracking das pecas usadas(melhor?)
 %   (para saber que pecas falta colocar no check/2).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/**************************************************
+% devolve os elemns que ja foram adicionados ao tab
+k(X) :- todasPecas(Y), ! , membro(X, Y), X.
+**************************************************/
