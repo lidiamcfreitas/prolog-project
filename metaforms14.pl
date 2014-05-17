03a7af26a5792363363a440b6c690ad1a917d883
iluxonchik <iluxon4ik@hotmail.com>
2014-05-17 12:34:03 +0100

    metaforms14.pl updates

--- metaforms14.pl
+++ metaforms14.pl
@@ -11,55 +11,63 @@
 
 % ESCREVER AQUI O CODIGO DO PROJECTO DE LP 2013/2014
 
-% predicados auxiliares
+['metaforms12.pl'].
+
+
+
 membro(X, [X|_]).
 membro(X, [_|R]) :- membro(X, R).
 
-% lista das precas disponiveis
-todasPecas([peca(triangulo, azul), peca(circulo, amarelo), peca(circulo, azul),peca(triangulo, amarelo), peca(triangulo, vermelho), peca(quadrado, vermelho), 
- peca(quadrado, amarelo), peca(quadrado, azul), peca(circulo, vermelho)]).
-
-% adiciona a peca as "lista" das pecas usadas
-adicionaPecaUsadas(X):- assert(X).
-
-% mostra as pecas que ainda nao foram usadas
-pecasNaoUsadas(X) :- todasPecas(Y), ! , membro(X, Y), not(X).
-
-% o predicado coloca/4 coloca a peca numa posicao do tabuleiro
-coloca(A, top, left, [A,_,_,_,_,_,_,_,_]).
-coloca(A, top, middle, [_,A,_,_,_,_,_,_,_]).
-coloca(A, top, right, [_,_,A,_,_,_,_,_,_]).
-coloca(A, center, left, [_,_,_,A,_,_,_,_,_]).
-coloca(A, center, middle, [_,_,_,_,A,_,_,_,_]).
-coloca(A, center, right, [_,_,_,_,_,A,_,_,_]).
-coloca(A, bottom, left, [_,_,_,_,_,_,A,_,_]).
-coloca(A, bottom, middle, [_,_,_,_,_,_,_,A,_]).
-coloca(A, bottom, right, [_,_,_,_,_,_,_,_,A]).
 
 
-% implementacoes das pistas
-trioLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-trioLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-cobra(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-tSimples(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-tLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-tRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-tInvertido(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-cantoTopLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-cantoTopRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-cantoBottomLeft(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-cantoBottomRight(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-diagonalGrave(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
-diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :- coloca(Peca, Linha, Coluna, Tabuleiro), adicionaPecaUsadas(Peca).
+trioLeft(Peca, Linha, Coluna, Tabuleiro) :- rectanguloVertical(Peca, Linha, Coluna, Tabuleiro).
+
+trioRight(Peca, Linha, Coluna, Tabuleiro):- rectanguloVertical(Peca, Linha, Coluna, Tabuleiro).
+
+cobra(Peca, Linha, Coluna, Tabuleiro) :- rectanguloVertical(Peca, Linha, Coluna, Tabuleiro).
+
+tLeft(Peca, Linha, Coluna, Tabuleiro) :- rectanguloVertical(Peca, Linha, Coluna, Tabuleiro).
+
+tRight(Peca, Linha, Coluna, Tabuleiro) :- rectanguloVertical(Peca, Linha, Coluna, Tabuleiro).
+
+tSimples(Peca, Linha, Coluna, Tabuleiro) :- rectanguloHorizontal(Peca, Linha, Coluna, Tabuleiro).
+
+tInvertido(Peca, Linha, Coluna, Tabuleiro) :- rectanguloHorizontal(Peca, Linha, Coluna, Tabuleiro).
+
+diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :- quadrado(Peca, Linha, Coluna, Tabuleiro).
+
+diagonalGravePeca, Linha, Coluna, Tabuleiro) :- quadrado(Peca, Linha, Coluna, Tabuleiro).
+
+cantoTopLeft(Peca, Linha, Coluna, Tabuleiro) :- quadrado(Peca, Linha, Coluna, Tabuleiro).
+
+cantoTopRight(Peca, Linha, Coluna, Tabuleiro) :- quadrado(Peca, Linha, Coluna, Tabuleiro).
+
+cantoBottomLeft(Peca, Linha, Coluna, Tabuleiro) :- quadrado(Peca, Linha, Coluna, Tabuleiro).
+
+cantoBottomRight(Peca, Linha, Coluna, Tabuleiro) :- quadrado(Peca, Linha, Coluna, Tabuleiro).
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% TODO:
-% - ter uma lista que contem todas as pecas possiveis, sempre que
-%   uma peca e adicionada ao tabuleiro, remove-la dessa lista	
-%	|OU| uma lista que faca o tracking das pecas usadas(melhor?)
-%   (para saber que pecas falta colocar no check/2).
+%
+%       PREDICADO CHECK
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-/**************************************************
-% devolve os elemns que ja foram adicionados ao tab
-k(X) :- todasPecas(Y), ! , membro(X, Y), X.
-**************************************************/
+peca(triangulo, vermelho).
+peca(triangulo, azul).
+peca(triangulo, amarelo).
+peca(quadrado, vermelho).
+peca(quadrado, azul).
+peca(quadrado, amarelo).
+peca(circulo, vermelho).
+peca(circulo, azul).
+peca(circulo, amarelo).
+
+
+check(Tabuleiro, TabuleiroCompleto):- check(Tabuleiro, TabuleiroCompleto, [peca(triangulo, vermelho),peca(triangulo, azul),peca(triangulo, amarelo),peca(quadrado, vermelho),peca(quadrado, azul),peca(quadrado, amarelo),peca(circulo, vermelho),peca(circulo, azul),peca(circulo, amarelo)]).
+
+check(Tabuleiro, TabuleiroCompleto, [X|R]) :-  membro(X, Tabuleiro), check(Tabuleiro, TabuleiroCompleto, R).
+
+
+
+
